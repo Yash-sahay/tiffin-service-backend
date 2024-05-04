f9f3bee927b772a652e907356f2d959566b5a77e
Adarsh <adarsh.gokula@gmail.com>
2024-05-04 23:53:50 +0530

    new db connected

--- db/config.js
+++ db/config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/test-series");
+mongoose.connect("mongodb+srv://ravikarsahay55:oE9A9pz7o8dKli0t@tiffin-service-database.sxx5l35.mongodb.net/?retryWrites=true&w=majority&appName=tiffin-service-database");
